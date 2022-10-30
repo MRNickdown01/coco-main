bd5ab92d7e9f447f76f3a67759584d518ec5b20d
viruop <virajrai706@gmail.com>
2022-10-30 21:39:22 +0530

    login ..

--- src/Pages/CustomerLogin/Login.js
+++ src/Pages/CustomerLogin/Login.js
@@ -3,7 +3,12 @@ import React from "react";
 import { useState, useEffect } from "react";
 import pencil from "../CoustomerRegister/assets/pencil.png";
 import "../HomePages/Style.css";
+import { useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+
 const Login = () => {
+  const history = useHistory();
+  const location = useLocation();
   const [state, setState] = useState(false);
   const [edit, setEdit] = useState(true);
   const [user, setUser] = useState({
@@ -12,6 +17,35 @@ const Login = () => {
       error: false,
     },
   });
+
+  useEffect(() => {
+    let locParams = location.search;
+    locParams = locParams.split("?token=")[1];
+    console.log(locParams, "abcd");
+    var requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    fetch(
+      `https://coco-backend1.herokuapp.com/getUserByToken/${locParams}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        let user = result.user;
+        console.log(result);
+        localStorage.setItem("authUser", JSON.stringify(user));
+        if (user.fullName) {
+          history.push(`/Dashboard?token=${locParams}`);
+        } else {
+          history.push("/completeprofile");
+        }
+      })
+      .catch((error) => console.log("error", error));
+  }, []);
 
   const onInputChange = (id, value) => {
     let _user = { ...user };
